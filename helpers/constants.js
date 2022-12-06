fe26c1ba992b95528ab4057279e0d90260bf77aa
franFanton-4R <francisco.fanton@4rsoluciones.com>
2022-12-06 12:24:37 -0300

    add file constants

---
+++ helpers/constants.js
@@ -0,0 +1,8 @@
+import { Dimensions } from "react-native";
+
+export const PLATFORM_IOS = "ios";
+export const PLATFORM_ANDROID = "android";
+export const WINDOW_DIMENSION = Dimensions.get("window");
+export const SCREEN_DIMENSION = Dimensions.get("screen");
+export const WIDTH_SCREEN = Dimensions.get("screen").width;
+export const HEIGHT_SCREEN = Dimensions.get("screen").height;
