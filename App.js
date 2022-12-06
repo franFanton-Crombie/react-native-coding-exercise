5da926c623d130c6b809e704aef871fd56ba204c
franFanton-4R <francisco.fanton@4rsoluciones.com>
2022-12-06 15:12:54 -0300

    add apollo provider

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { StyleSheet, View } from "react-native";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "./helpers/colors";
 import MainScreen from "./screens/MainScreen";
 
 export default function App() {
+  const client = new ApolloClient({
+    uri: "https://api.spacex.land/graphql",
+    cache: new InMemoryCache(),
+  });
   return (
-    <SafeAreaView style={styles.container}>
-      <MainScreen />
-    </SafeAreaView>
+    <ApolloProvider client={client}>
+      <SafeAreaView style={styles.container}>
+        <MainScreen />
+      </SafeAreaView>
+    </ApolloProvider>
   );
 }
 
